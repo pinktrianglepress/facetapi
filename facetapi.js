d382a4a0c3943ae1ce5ad24f151fefe630838ecc
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2010-05-28 18:48:52 +0000

    Fixed the soft limit settings for facets in the multiblock realm.

--- facetapi.js
+++ facetapi.js
@@ -1,17 +1,29 @@
 // $Id$
 
 Drupal.behaviors.facetapi = function(context) {
+  
+  // Iterates over the settings array for each facet in each realm for each
+  // searcher module.
   $.each(Drupal.settings.facetapi, function (searcher, realms) {
     $.each(Drupal.settings.facetapi[searcher], function (realm_name, facets) {
-      $.each(Drupal.settings.facetapi[searcher][realm_name].$, function (facet_name, settings) {
+      $.each(Drupal.settings.facetapi[searcher][realm_name], function (facet_name, settings) {
+        
+        // A limit of 0 means no limit.
         if (settings.limit > 0) {
+          
+          // Hides all items that are past the "soft limit".
           limit=settings.limit - 1;
           $('div#block-facetapi-'+searcher+'-'+realm_name+'-'+facet_name+' ul').find('li:gt('+limit+')').hide();
+          
+          // Finds all items that have more values than our soft limit allows
+          // for, adds the show more/less links as appropriate.
           $('div#block-facetapi-'+searcher+'-'+realm_name+'-'+facet_name+' ul').filter(function() {
             return $(this).find('li').length > settings.limit;
           }).each(function() {
-            $('<a href="#" class="facetapi-hide-link'+searcher+'"></a>').text(Drupal.t('Show more')).click(function() {
-              cur_limit=Drupal.settings.facetapi[$(this).attr('class').substring(21)]['limit'] - 1;
+            $('<a href="#" class="facetapi-hide-link"></a>').text(Drupal.t('Show more')).click(function() {
+              
+              // Gets the limit for the facet, adds show more/less link.
+              cur_limit = Drupal.settings.facetapi[searcher][realm_name][facet_name].limit - 1;
               if ($(this).prev().find('li:hidden').length > 0) {
                 $(this).prev().find('li:gt('+cur_limit+')').slideDown();
                 $(this).text(Drupal.t('Show less'));
