9f19cd706668cf7e0b43838da5a53d4382f907c1
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2010-05-28 22:21:56 +0000

    Added widget functionality, ported over checkbox facets.

--- facetapi.js
+++ facetapi.js
@@ -8,12 +8,25 @@ Drupal.behaviors.facetapi = function(context) {
     $.each(Drupal.settings.facetapi[searcher], function (realm_name, facets) {
       $.each(Drupal.settings.facetapi[searcher][realm_name], function (facet_name, settings) {
         
+        // Converts to checkboxes if 
+        if (Drupal.settings.facetapi[searcher][realm_name][facet_name].widget == 'facetapi_checkboxes') {
+          $('div#block-facetapi-'+searcher+'-'+realm_name+'-'+facet_name+' ul li a:not([class=active])')
+            .each(Drupal.facetapi.addCheckbox)
+          $('div#block-facetapi-'+searcher+'-'+realm_name+'-'+facet_name+' ul li a.active')
+            .each(Drupal.facetapi.addActiveCheckbox)
+        }
+        
+        
+        
+        
         // A limit of 0 means no limit.
         if (settings.limit > 0) {
           
           // Hides all items that are past the "soft limit".
           limit=settings.limit - 1;
-          $('div#block-facetapi-'+searcher+'-'+realm_name+'-'+facet_name+' ul:first').find('li:gt('+limit+')').hide();
+          $('div#block-facetapi-'+searcher+'-'+realm_name+'-'+facet_name+' ul:first')
+            .find('li:gt('+limit+')')
+            .hide();
           
           // Finds all items that have more values than our soft limit allows
           // for, adds the show more/less links as appropriate.
@@ -40,3 +53,31 @@ Drupal.behaviors.facetapi = function(context) {
     });
   });
 };
+
+// Checkbox building methods.
+Drupal.facetapi = {}
+
+Drupal.facetapi.addCheckbox = function() {
+  // Put href in context scope to be visible in the anonymous function.
+  var href = $(this).attr('href');
+  $(this).before($('<input type="checkbox" />')
+    .attr('class', 'facetapi-checkbox')
+    .click(function(){
+      window.location.href = href;
+    })
+  );
+}
+
+Drupal.facetapi.addActiveCheckbox = function() {
+  // Create a checked checkbox.
+  var checkbox = $('<input type="checkbox" />')
+    .attr('class', 'facetapi-checkbox')
+    .attr('checked', true);
+  // Put href in context scope to be visible in the anonymous function.
+  var href = $(this).attr('href');
+  checkbox.click(function(){
+    window.location.href = href;
+  });
+  // Add the checkbox.
+  $(this).before(checkbox);
+}
