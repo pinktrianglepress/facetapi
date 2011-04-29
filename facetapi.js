12ce0ab4750f31e36786e1b5c5d8403eac29c791
Chris Pliakas <cpliakas@266779.no-reply.drupal.org>
2011-04-29 12:44:00 -0400

    Issue #1127268 by cpliakas: Ported soft limit functionality.

--- facetapi.js
+++ facetapi.js
@@ -2,30 +2,54 @@
 
 Drupal.behaviors.facetapi = {
   attach: function(context, settings) {
-    $('.facetapi-hidden-facet', context).hide();
-    $('<a href="#" class="facetapi-showhide"></a>').text(Drupal.t('Show more')).click(function() {
-      if ($(this).parent().find('.facet-hidden:visible').length == 0) {
-        $(this).parent().find('.facet-hidden').show();
-        $(this).text(Drupal.t('Show fewer'));
+    // Iterates over facet settings, applies functionality like the "Sore more"
+    // links for block realm facets.
+    // @todo We need some sort of JS API so we don't have to make decisions
+    // based on the realm.
+    for (var index in Drupal.settings.facetapi.facets) {
+      if ('block' == settings.facetapi.facets[index].realmName) {
+        Drupal.facetapi.applyLimit(settings.facetapi.facets[index]);
       }
-      else {
-        $(this).parent().find('.facet-hidden').hide();
-        $(this).text(Drupal.t('Show more'));
-      }
-      return false;
-    }).appendTo($(settings.apachesolr_show_more_blocks, context));
-
-    // Find all checkbox facet links and give them a checkbox
-    $('a.facet-checkbox.facet-click', context).each(Drupal.facetapi.addCheckbox);
-    // Find all unclick links and turn them into checkboxes
-    $('a.facet-checkbox.facet-unclick', context).each(Drupal.facetapi.makeCheckbox);
+    }
   }
 }
 
+/**
+ * Class containing functionality for Facet API.
+ */
 Drupal.facetapi = {}
 
 /**
- * Constructor for a class.
+ * Applies the soft limit to facets in the block realm.
+ */
+Drupal.facetapi.applyLimit = function(settings) {
+  if (settings.limit > 0) {
+    
+    // Ensures our limit is zero-based, hides facets over the limit.
+    var limit = settings.limit - 1;
+    $('ul#' + settings.id).find('li:gt(' + limit + ')').hide();
+    
+    // Adds "Show more" / "Show fewer" links as appropriate.
+    $('ul#' + settings.id).filter(function() {
+      return $(this).find('li').length > settings.limit;
+    }).each(function() {
+      $('<a href="#" class="facetapi-limit-link"></a>').text(Drupal.t('Show more')).click(function() {
+        if ($(this).prev().find('li:hidden').length > 0) {
+          $(this).prev().find('li:gt(' + limit + ')').slideDown();
+          $(this).text(Drupal.t('Show fewer'));
+        }
+        else {
+          $(this).prev().find('li:gt(' + limit + ')').slideUp();
+          $(this).text(Drupal.t('Show more'));
+        }
+        return false;
+      }).insertAfter($(this));
+    });
+  }
+}
+
+/**
+ * Constructor for the facetapi redirect class.
  */
 Drupal.facetapi.Redirect = function(href) {
   this.href = href;
